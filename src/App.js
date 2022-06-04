845cbb7cc786ed5fe87af7c6dccfac1d8a07d8e9
acilegna <acilegna.airam88@gmail.com>
2022-06-04 11:50:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import {faGlobe} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {Navbar} from 'components';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <FontAwesomeIcon icon={faGlobe} />
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/mentor" />
+        <Route path=" " />
+        <Route path=" " />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
