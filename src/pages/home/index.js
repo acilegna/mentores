37cf9f31b02586896882083ad115fa84981934c8
acilegna <acilegna.airam88@gmail.com>
2022-09-07 13:42:09 -0500

    responsive home-mentor

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,33 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.css';
 import {Parallax} from 'react-parallax';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import imagen from './developer-coding-on-laptop.jpg';
 
+import Button from 'react-bootstrap/Button';
+
+function simulateNetworkRequest() {
+  return new Promise(resolve => setTimeout(resolve, 2000));
+}
 export const Home = () => {
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (isLoading) {
+      simulateNetworkRequest().then(() => {
+        setLoading(false);
+      });
+    }
+  }, [isLoading]);
+
+  const handleClick = () => setLoading(true);
+  /*para boton*/
+
   return (
     <div style={{textAlign: 'center'}}>
-      <Parallax
-        bgImage={imagen}
-        strength={200}
-        renderLayer={precentage => (
-          <div
-            style={{
-              position: 'absolute',
-              width: '40px',
-              height: '15px',
-              borderRadius: '10%',
-              background: `rgba(255, 255, 255, ${precentage * 1})`,
-              left: '50%',
-              top: '50%',
-              transform: `translate(-50%, -50%) scale(${precentage * 3})`,
-            }}>
-            <a className="text-button-mentor" href=" ">
-              Buscar
-            </a>
-          </div>
-        )}>
-        <div style={{height: 400}}>
-          <h1 id="mentor">ENCUENTRA AQUI TU MENTOR</h1>
+      <Parallax bgImage={imagen} strength={200}>
+        <div className="cont-mentor" style={{height: 400}}>
+          <Container fluid>
+            <Row>
+              <Col>
+                <h1 id="mentor">ENCUENTRA AQUÍ TU MENTOR</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <h2 id="help">
+                  incrementa tu potencial con ayuda de nuestros{' '}
+                  <strong>Mentores</strong>
+                </h2>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Button
+                  id="btns-primary"
+                  size="sm"
+                  disabled={isLoading}
+                  onClick={!isLoading ? handleClick : null}>
+                  {isLoading ? 'Loading…' : 'Ver mentores'}
+                </Button>
+              </Col>
+            </Row>
+          </Container>
         </div>
       </Parallax>
       <div style={{height: '30vh'}}></div>
